08a6f2d1f44eaa2e365000573f2b0fe9a4dccd6c
lucas <lucaslejeune0@gmail.com>
2024-12-17 16:45:26 +0100

    Manage Duck and Dog creation

--- src/main/java/com/example/AnimalFactory.java
+++ src/main/java/com/example/AnimalFactory.java
@@ -4,6 +4,10 @@ public class AnimalFactory {
     public static Animal createAnimal(String type, String name) {
         if ("Chat".equalsIgnoreCase(type)) {
             return new Cat(name);
+        } else if ("Chien".equalsIgnoreCase(type)) {
+            return new Cat(name);
+        } else if ("Canard".equalsIgnoreCase(type)) {
+            return new Cat(name);
         }
         throw new IllegalArgumentException("Cet animal n'est pas paramétré");
     }
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -5,5 +5,13 @@ public class Main {
         Animal chat = AnimalFactory.createAnimal("Chat", "Felix");
         System.out.println("Animal crée: " + chat.getName());
         System.out.println(chat.animalScream());
+
+        Animal chien = AnimalFactory.createAnimal("Chien", "Maurice");
+        System.out.println("Animal crée: " + chien.getName());
+        System.out.println(chien.animalScream());
+
+        Animal canard = AnimalFactory.createAnimal("Canard", "Louis");
+        System.out.println("Animal crée: " + canard.getName());
+        System.out.println(canard.animalScream());
     }
 }
